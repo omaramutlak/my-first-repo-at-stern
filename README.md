410b31797f02e6bef7107842253f5e42a063f144
omaramutlak <omarmutlak@gmail.com>
2025-07-29 19:46:35 -0400

    Added a test and configured pytest

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
-# my-first-repo-at-stern
+# my-first-repo-summer-2025
 
+## Setup
 
-I added this sentence using the GitHub online interface to edit the file.
+Create and activate a virtual environment:
 
-This is the README.md file. It uses the markdown language.
+```sh
+conda create -n my-first-env-2025 python=3.11
 
-Here is a list:
+conda activate my-first-env-2025
+```
 
-+ Item 1
-+ Item 2
-+ Item 3
+Install packages:
 
+```sh
+# pip install pytest
+
+pip install -r requirements.txt
+```
+
+## Usage
+
+Play a game of rock, paper scissors:
+
+```sh
+# only works if this file does NOT import from other local py files:
+python app/rps.py
+
+# if this file imports from other local py files:
+python -m app.rps
+```
+
+## Tests
+
+Run the tests:
+
+```sh
+# find all the tests and run them:
+pytest
+```
--- app/rps.py
+++ app/rps.py
@@ -14,6 +14,24 @@ VALID_OPTIONS = ["rock", "paper", "scissors"]
 computer_choice = random.choice(VALID_OPTIONS)
 print("COMPUTER CHOSE:", computer_choice)
 
-# todo: determine the winner
+def determine_winner(u, c):
+    if u == c:
+        result = "TIE GAME"
+    elif u == "rock" and c == "scissors":
+        result = "USER WINS"
+    elif u == "rock" and c == "paper":
+        result = "COMP WINS"
+    elif u == "scissors" and c == "rock":
+        result = "COMP WINS"
+    elif u == "scissors" and c == "paper":
+        result = "USER WINS"
+    elif u == "paper" and c == "rock":
+        result = "COMP WINS"
+    elif u == "paper" and c == "scissors":
+        result = "COMP WINS"
+    return result
 
-print("WINNER: TODO")
+result_message = determine_winner(player_choice, computer_choice)
+print(result_message)
+
+
